5437065556472935c28bcc01b153fb03b31914e9
meihui03 <mlok0006@student.monash.edu>
2025-09-26 17:20:33 +0800

    update size of sidebar

--- popules/src/App.vue
+++ popules/src/App.vue
@@ -11,13 +11,20 @@ export default {
 
 <template>
   <div class="min-h-screen bg-white flex flex-col">
+    <!-- Header -->
     <Header />
+
     <div class="flex flex-1">
-      <Sidebar />
+      <!-- Sidebar -->
+      <aside class="w-64 h-screen sticky top-0 border-r overflow-auto">
+        <Sidebar />
+      </aside>
+
+      <!-- Main content -->
       <div class="flex-1 flex flex-col">
         <CategoryTabs />
-        <main class="flex-1 p-6 ">
-          <MainContent/>
+        <main class="flex-1 p-6 overflow-auto">
+          <MainContent />
         </main>
       </div>
     </div>
