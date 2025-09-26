8c071202b80798f243270c8b04dda016aa96dc0d
meihui03 <mlok0006@student.monash.edu>
2025-09-26 14:23:01 +0800

    add category tabs

--- popules/src/App.vue
+++ popules/src/App.vue
@@ -1,22 +1,24 @@
 <script>
 import Header from './components/Header.vue'
 import Sidebar from './components/Sidebar.vue'
+import CategoryTabs from './components/CategoryTabs.vue';
 
 export default {
-  components: { Header, Sidebar }
+  components: { Header, Sidebar ,CategoryTabs }
 }
 </script>
 
 <template>
   <div class="min-h-screen bg-white flex flex-col">
     <Header />
-
-    <div class="flex">
+    <div class="flex flex-1">
       <Sidebar />
-
-      <main class="flex-1 p-6 bg-gray-50">
-        <h1 class="text-xl font-semibold">Main Content Area</h1>
-      </main>
+      <div class="flex-1 flex flex-col">
+        <CategoryTabs />
+        <main class="flex-1 p-6 ">
+          <h1 class="text-xl font-semibold">Main Content Area</h1>
+        </main>
+      </div>
     </div>
   </div>
 </template>
---
+++ popules/src/components/CategoryTabs.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'CategoryTabs'
+}
+</script>
+
+<template>
+  <div class="w-full bg-white">
+    <div class="sticky flex gap-1 overflow-x-auto scrollbar-hide px-5 py-3 bg-white">
+      <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        All
+      </button>
+      <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer  ">
+        Vacancies
+      </button>
+      <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Workplace
+      </button>
+      <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Food
+      </button>
+       <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Design
+      </button>
+       <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Cars
+      </button>
+      <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Finance
+      </button>
+      <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Lifestyle
+      </button>
+       <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Travel
+      </button>
+       <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Makeup
+      </button>
+       <button class="px-4 py-2 text-gray-500 rounded-full hover:bg-gray-100 cursor-pointer ">
+        Fitness
+      </button>
+    </div>
+  </div>
+</template>
